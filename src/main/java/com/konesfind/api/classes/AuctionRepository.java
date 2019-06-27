283fbc296b4f6cdc81afd41ae52ac9a27b780ba3
וכמן <@DESKTOP-TLMTSE0>
2019-06-28 00:23:03 +0300

    second commit - null error on find

--- src/main/java/com/konesfind/api/classes/AuctionRepository.java
+++ src/main/java/com/konesfind/api/classes/AuctionRepository.java
@@ -1,9 +1,20 @@
 package com.konesfind.api.classes;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import com.konesfind.api.classes.Auction;
+
+
+
 @Repository
-public interface AuctionRepository extends JpaRepository<Auction, Long> {
-	
+	@RepositoryRestResource(collectionResourceRel = "auctions", path = "auctions")
+	public interface AuctionRepository extends PagingAndSortingRepository<Auction,Long> {
+		List<Auction> findByauctionName(@Param("auctionName") String auctionName);
+		List<Auction> findByclosingAt(@Param("closingAt") String closingAt);
+		List<Auction> findAll();
 }
