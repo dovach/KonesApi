cbf24661e48f3c022a91aeb1fc2d0e4a5e91db4e
dov <dov@EPR-150-Dov.es.local>
2019-06-27 23:32:50 +0300

    KonesApi first commit

---
+++ src/main/java/com/konesfind/api/classes/Auction.java
@@ -0,0 +1,131 @@
+package com.konesfind.api.classes;
+
+import java.util.Currency;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+//import org.hibernate.validator.constraints.Currency;
+import org.springframework.data.annotation.CreatedDate;
+import javax.persistence.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "auction")
+@EntityListeners(AuditingEntityListener.class)
+public class Auction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+    
+    @Column(name = "auction_name", nullable = false)
+    private String auctionName;
+    
+    @Column(name = "product_type", nullable = false)
+    private String productType;
+    
+    @Column(name = "created_at", nullable = false)
+    @CreatedDate
+    private Date createdAt;
+    
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private Date updatedAt;
+    
+    @Column(name = "updated_by", nullable = false)
+    @LastModifiedBy
+    private String updatedBy;
+    
+    @Column(name = "closing_at", nullable = false)
+    private Date closingAt;
+
+    @Column(name = "base_price", nullable = false)
+    private Currency BasePrice;
+    
+    @Column(name = "current_price", nullable = false)
+    private Currency CurrentPrice;
+    
+    Auction(){}
+    
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAuctionName() {
+		return auctionName;
+	}
+
+	public void setAuctionName(String auctionName) {
+		this.auctionName = auctionName;
+	}
+
+	public String getProductType() {
+		return productType;
+	}
+
+	public void setProductType(String productType) {
+		this.productType = productType;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Date getClosingAt() {
+		return closingAt;
+	}
+
+	public void setClosingAt(Date closingAt) {
+		this.closingAt = closingAt;
+	}
+
+	public Currency getBasePrice() {
+		return BasePrice;
+	}
+
+	public void setBasePrice(Currency basePrice) {
+		BasePrice = basePrice;
+	}
+
+	public Currency getCurrentPrice() {
+		return CurrentPrice;
+	}
+
+	public void setCurrentPrice(Currency currentPrice) {
+		CurrentPrice = currentPrice;
+	}
+    
+}
+    //תאריך פרסום, תאריך סגירה, פרטי המוצר, מחיר התחלתי, הצעה מקסימלית ידועה
---
+++ src/main/java/com/konesfind/api/classes/AuctionController.java
@@ -0,0 +1,102 @@
+package com.konesfind.api.classes;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AuctionController {
+  @Autowired
+  private AuctionRepository auctionrRepository;
+  /**
+   * Get all auctions list.
+   *
+   * @return the list
+   */
+  @GetMapping("/auctions")
+  public List<Auction> getAllAuctions() {
+    return auctionrRepository.findAll();
+  }
+  /**
+   * Gets auctions by id.
+   *
+   * @param auctionId the auction id
+   * @return the auctions by id
+   * @throws ResourceNotFoundException the resource not found exception
+   */
+  @GetMapping("/auctions/{id}")
+  public ResponseEntity<Auction> getAuctionsById(@PathVariable(value = "id") Long auctionId)
+      throws ResourceNotFoundException {
+	  Auction auction =
+        auctionrRepository
+            .findById(auctionId)
+            .orElseThrow(() -> new ResourceNotFoundException("Auction not found on :: " + auctionId));
+    return ResponseEntity.ok().body(auction);
+  }
+  /**
+   * Create auction auction.
+   *
+   * @param auction the auction
+   * @return the auction
+   */
+  @PostMapping("/auctions")
+  public Auction createAuction(@Valid @RequestBody Auction auction) {
+    return auctionrRepository.save(auction);
+  }
+//  /**
+//   * Update auction response entity.
+//   *
+//   * @param auctionId the auction id
+//   * @param auctionDetails the auction details
+//   * @return the response entity
+//   * @throws ResourceNotFoundException the resource not found exception
+//   */
+//  @PutMapping("/auctions/{id}")
+//  public ResponseEntity<Auction> updateAuction(
+//      @PathVariable(value = "id") Long auctionId, @Valid @RequestBody Auction auctionDetails)
+//      throws ResourceNotFoundException {
+//    Auction auction =
+//        auctionrRepository
+//            .findById(auctionId)
+//            .orElseThrow(() -> new ResourceNotFoundException("Auction not found on :: " + auctionId));
+//    auction.setAuctionName(auctionDetails.getAuctionName());
+//    auction.setProductType(auctionDetails.getProductType());
+//    auction.setCurrentPrice(auctionDetails.getCurrentPrice());
+//    auction.setUpdatedAt(new Date());
+//    final Auction updatedAuction = auctionrRepository.save(auction);
+//    return ResponseEntity.ok(updatedAuction);
+//  }
+//  /**
+//   * Delete auction map.
+//   *
+//   * @param auctionId the auction id
+//   * @return the map
+//   * @throws Exception the exception
+//   */
+//  @DeleteMapping("/auction/{id}")
+//  public Map<String, Boolean> deleteAuction(@PathVariable(value = "id") Long auctionId) throws Exception {
+//    Auction auction =
+//        auctionrRepository
+//            .findById(auctionId)
+//            .orElseThrow(() -> new ResourceNotFoundException("Auction not found on :: " + auctionId));
+//    auctionrRepository.delete(auction);
+//    Map<String, Boolean> response = new HashMap<>();
+//    response.put("deleted", Boolean.TRUE);
+//    return response;
+//  }
+}
